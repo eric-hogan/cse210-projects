a901655baab3cf6d04e50c439b264286aa155d18
eric-hogan <hog.eric@hotmail.com>
2025-07-02 18:41:06 -0600

    push attempt 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomeGenerator = new Random();
+        int number = randomeGenerator.Next(1, 5);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessNum = int.Parse(guess);
+
+        int guessCount = 1;
+        string playAgain = "yes";
+
+        do
+        {
+            while (guessNum != number)
+            {
+                if (guessNum < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guessNum > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                guessNum = int.Parse(guess);
+
+                guessCount += 1;
+
+            }
+
+            Console.WriteLine($"Great Job. You guessed the magic number in {guessCount} guess(es)!");
+        } while 
     }
 }
