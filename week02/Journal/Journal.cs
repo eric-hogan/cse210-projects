adcb9af2fd5d813660b7f1cdff26f5a812ade9cd
eric-hogan <hog.eric@hotmail.com>
2025-07-11 20:23:30 -0600

    stubs for Journal project

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,25 @@
+using System.Configuration.Assemblies;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+    
+    }
+}
