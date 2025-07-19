57f47a151a67dd891344b733899619b5edc5ae85
eric-hogan <hog.eric@hotmail.com>
2025-07-19 11:03:31 -0600

    desing complete

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,13 +6,16 @@ class Program
     static void Main(string[] args)
     {
 
-        // Reference r1 = new Reference("John", 3, 16);
-        // Reference r2 = new Reference("Alma", 7,16, 17);
+        Reference r1 = new Reference("John", 3, 16);
+        Reference r2 = new Reference("Alma", 7, 16, 17);
 
-        // Console.WriteLine(r1.GetDisplayText());
-        // Console.WriteLine(r2.GetDisplayText());
+        Console.WriteLine(r1.GetDisplayText());
+        Console.WriteLine(r2.GetDisplayText());
 
-        Word w1 = new Word("hello there");
+        // Word w1 = new Word("hello there");
+
+        Scripture s1 = new Scripture(r1, "this is the scripture");
+        Console.WriteLine(s1.GetDisplayText());
 
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -3,5 +3,24 @@ public class Scripture
     Reference _reference;
     List<Word> _words;
 
-    
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        Console.WriteLine($"{reference} {text}");
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "program complete";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
 }
